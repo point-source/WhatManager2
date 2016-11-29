b7681ed844cca6cd267ee4b61c8e1a58ef6f5aa4
MADindustries <spamayl@gmail.com>
2016-11-28 19:33:23 -0800

    Added new mktorrent to setup script
    its a bit hackish

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
-sudo apt-get install -y libapache2-mod-wsgi python-pip flac lame sox mktorrent libmysqlclient-dev python-dev libxml2-dev libxslt1-dev curl
+sudo apt-get install -y libapache2-mod-wsgi python-pip flac lame sox libmysqlclient-dev python-dev libxml2-dev libxslt1-dev curl
+
+sudo git clone https://github.com/Rudde/mktorrent.git
+cd ./mktorrent/
+sudo make
+sudo chmod +x ./mktorrent
+sudo cp ./mktorrent /usr/bin/mktorrent
+cd ../
+sudo rm -r ./mktorrent
 
 sudo pip install -r requirements.txt
 sudo chmod 777 media/book_data
