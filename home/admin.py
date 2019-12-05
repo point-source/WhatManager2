159365cfbbf0c568db6ee98b5e7ff0f406cde96c
PointSource <47544779+point-source@users.noreply.github.com>
2019-12-04 18:58:06 -0800

    Enable editing of TransInstance objects from admin page

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from home.models import DownloadLocation, ReplicaSet
+from home.models import DownloadLocation, ReplicaSet, TransInstance
 
 admin.site.register(DownloadLocation)
 admin.site.register(ReplicaSet)
+admin.site.register(TransInstance)
