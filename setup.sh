f1b9e2c04e09dc4f9e27cbe00e1e06a8d6db45fa
PointSource <47544779+point-source@users.noreply.github.com>
2019-12-16 15:20:39 -0800

    Set permissions on entire media directory during setup

--- setup.sh
+++ setup.sh
@@ -8,6 +8,4 @@ sudo pip3 install pipenv
 export PIPENV_VENV_IN_PROJECT=True
 pipenv install --three
 
-sudo chmod 777 media/book_data
-sudo chmod 777 media/what_image_cache
-sudo chmod 777 media/qobuz_uploads
+sudo chmod 777 -R media
