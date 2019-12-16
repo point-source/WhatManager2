6e250f6bf5764ce33409cbe4874101f3c318229e
PointSource <47544779+point-source@users.noreply.github.com>
2019-12-16 15:36:29 -0800

    No sudo needed in containers

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@
 
 cp -i WhatManager2/user_settings.example.py WhatManager2/user_settings.py
 
-sudo apt install -y python3-pip flac lame sox mktorrent curl python3-dev libssl-dev
+apt install -y python3-pip flac lame sox mktorrent curl python3-dev libssl-dev
 
-sudo pip3 install pipenv
+pip3 install pipenv
 export PIPENV_VENV_IN_PROJECT=True
 pipenv install --three
 
-sudo chmod 777 -R media
+chmod 777 -R media
