f84ddbd4cfd36f07fda4964266d83b054d089502
PointSource <47544779+point-source@users.noreply.github.com>
2019-12-16 15:50:00 -0800

    Add git and libmysqlclient-dev to setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 cp -i WhatManager2/user_settings.example.py WhatManager2/user_settings.py
 
-apt install -y python3-pip flac lame sox mktorrent curl python3-dev libssl-dev
+apt install -y python3-pip flac lame sox mktorrent curl python3-dev libssl-dev git libmysqlclient-dev
 
 pip3 install pipenv
 export PIPENV_VENV_IN_PROJECT=True
